74cdd1cef2a1f51a6294347fc79f46b86e51a396
DesertZito <118901855+DesertZito@users.noreply.github.com>
2024-05-09 19:12:21 +0200

    Create Utils.java

---
+++ sdis/spotify/server/Utils.java
@@ -0,0 +1,10 @@
+package sdis.spotify.server;
+
+public class Utils {
+    public static void logMsg(String streamLog, String s) {
+    }
+
+    public static String nowDate() {
+        return null;
+    }
+}
