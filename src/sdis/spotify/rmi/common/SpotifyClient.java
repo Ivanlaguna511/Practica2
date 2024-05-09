cc49fa48aed1204c031ab43b5b47afe68e51c97e
DesertZito <118901855+DesertZito@users.noreply.github.com>
2024-05-09 19:34:23 +0200

    Update SpotifyClient.java

--- src/sdis/spotify/rmi/common/SpotifyClient.java
+++ src/sdis/spotify/rmi/common/SpotifyClient.java
@@ -4,7 +4,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface SpotifyClient extends Remote {
-    public boolean launchMediaPlayer(Media cancion);
-    public boolean isMediaPlayerActive() throws RemoteException; //Lanza excepción pero no se cual
-    public void startStream(Media cancion, String ipServerSocket, int puertoServer) throws RemoteException;
+     boolean launchMediaPlayer(Media cancion);
+     boolean isMediaPlayerActive() throws RemoteException; //Lanza excepción pero no se cual
+     void startStream(Media cancion, String ipServerSocket, int puertoServer) throws RemoteException;
 }
