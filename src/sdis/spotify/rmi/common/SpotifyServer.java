0562d970b1522f0efcbcf6c965bfea4877428905
DesertZito <118901855+DesertZito@users.noreply.github.com>
2024-05-09 19:34:12 +0200

    Update SpotifyServer.java

--- src/sdis/spotify/rmi/common/SpotifyServer.java
+++ src/sdis/spotify/rmi/common/SpotifyServer.java
@@ -5,7 +5,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface SpotifyServer extends Remote {
-    public boolean setClientStreamReceptor(SpotifyClient cliente) throws RemoteException;
-    public String randomPlay(); //Tiene que lanzar una excepción pero no se cúal
-    public String startMedia(Media cancion) throws FileNotFoundException; //Lanza otra excepción pero no se cual
+     boolean setClientStreamReceptor(SpotifyClient cliente) throws RemoteException;
+     String randomPlay(); //Tiene que lanzar una excepción pero no se cúal
+     String startMedia(Media cancion) throws FileNotFoundException; //Lanza otra excepción pero no se cual
 }
