bbb79a02c3e2520cd5df26e36e57006e312ba984
Ivanlaguna511 <118934677+Ivanlaguna511@users.noreply.github.com>
2024-05-14 17:38:40 +0200

    Update SpotifyClient.java

--- src/spotify/rmi/common/SpotifyClient.java
+++ src/spotify/rmi/common/SpotifyClient.java
@@ -6,7 +6,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface SpotifyClient extends Remote {
-    public boolean launchMediaPlayer(Media cancion);
+    public boolean launchMediaPlayer(Media cancion) throws RemoteException;
     public boolean isMediaPlayerActive() throws RemoteException;
     public void startStream(Media cancion, String ipServerSocket, int puertoServer) throws RemoteException;
 }
