5932cd199c8cca4a98421a08bf2c280ec0cb5716
hecctor16 <162744818+hecctor16@users.noreply.github.com>
2024-05-17 17:03:50 +0200

    Update DeleteL.java

--- src/spotify/rmi/client/directory/unit/DeleteL.java
+++ src/spotify/rmi/client/directory/unit/DeleteL.java
@@ -1,72 +1,27 @@
 package spotify.rmi.client.directory.unit;
 
-import java.util.Scanner;
+import spotify.rmi.common.Spotify;
+import spotify.media.Media;
+import java.rmi.Naming;
 
 public class DeleteL {
-    //Ejemplo
-    public static void main(String [ ] args) {
-        String host = (args.length < 1) ? null : args[0];
+    public static void main(String [] args) {
         try {
-            Hello or = (Hello) Naming.lookup("rmi://"+host+"/id1");
-            String respuesta = or.sayHello();
-            System.out.println("[Respuesta:"+respuesta+"]");
+            String host = "localhost";
+            Spotify or = (Spotify) Naming.lookup("rmi://" + host + "/id1");
+            if (args.length == 1) {
+                String playlist = args[0];
+                String respuesta = or.deleteL(playlist);
+                System.out.println("[Respuesta:  " + respuesta + ".");
+            } else {
+                System.out.println("Uso incorrecto: DeleteL [playlist].");
+            }
         } catch (java.rmi.RemoteException re) {
-            System.err.println("<Cliente: Excepción RMI:"+re);
+            System.err.println("<Cliente: Excepción RMI:" + re);
             re.printStackTrace();
-
         } catch (Exception e) {
-            System.err.println("<Cliente: Excepcion: "+e);
+            System.err.println("<Cliente: Excepcion: " + e);
             e.printStackTrace();
         }
-    }
-
-
-
-
-    //P1
-    final private int PUERTO = 2000;
-    static java.io.ObjectInputStream ois = null;
-    static java.io.ObjectOutputStream oos = null;
-    public static void main(String[] args) throws java.io.IOException {
-        Scanner s = new Scanner(System.in);
-        String [] array=new String[2];
-        array[0]="localhost";
-
-        String host = array[0];  //localhost o ip|dn del servidor
-
-        java.net.Socket sock = new java.net.Socket(host, 2000);
-        try {
-            oos = new java.io.ObjectOutputStream(sock.getOutputStream());
-            ois = new java.io.ObjectInputStream(sock.getInputStream());
-            MensajeProtocolo minfo = (MensajeProtocolo) ois.readObject();
-            System.out.println(minfo);
-            if(!minfo.getPrimitiva().equals(Primitiva.ERROR)){
-                System.out.println("introduzca la playlist que desea eliminar");
-                array[1]=s.nextLine();
-                String clave = array[1];
-                pruebaPeticionRespuesta(new MensajeProtocolo(Primitiva.DELETEL,
-                        clave));
-            }
-
-        } catch (java.io.EOFException e) {
-            System.err.println("Cliente: Fin de conexión.");
-        } catch (java.io.IOException e) {
-            System.err.println("Cliente: Error de apertura o E/S sobre objetos: "+e);
-        } catch (MalMensajeProtocoloException e) {
-            System.err.println("Cliente: Error mensaje Protocolo: "+e);
-        } catch (Exception e) {
-            System.err.println("Cliente: Excepción. Cerrando Sockets: "+e);
-        } finally {
-            ois.close();
-            oos.close();
-            sock.close();
-        }
-    }
-    static void pruebaPeticionRespuesta(MensajeProtocolo mp) throws
-            java.io.IOException,
-            MalMensajeProtocoloException, ClassNotFoundException {
-        System.out.println("> "+mp);
-        oos.writeObject(mp);
-        System.out.println("< "+(MensajeProtocolo) ois.readObject());
     }
 }
