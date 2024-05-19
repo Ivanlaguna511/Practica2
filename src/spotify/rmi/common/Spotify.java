2bb2766258d6ad145cde5c91c37f45d3c3efafb8
hecctor16 <162744818+hecctor16@users.noreply.github.com>
2024-05-19 19:58:43 +0200

    Update Spotify.java

--- src/spotify/rmi/common/Spotify.java
+++ src/spotify/rmi/common/Spotify.java
@@ -22,4 +22,7 @@ public interface Spotify extends Remote {
 
     public  String addScore(String nombreCancion, double puntuacion) throws RemoteException;
     public  String addComment(String nombreCancion, String comentario) throws RemoteException;
+
+    public  void addLike(String nombreCancion) throws RemoteException;
+    public  void tagAdultContent(String nombreCancion, boolean flag) throws RemoteException;
 }
