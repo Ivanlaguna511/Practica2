555f55f4decf87ef0941b04215197f84673eea73
hecctor16 <162744818+hecctor16@users.noreply.github.com>
2024-05-19 19:57:22 +0200

    Update CompleteMetadata.java

--- src/spotify/rmi/client/directory/unit/CompleteMetadata.java
+++ src/spotify/rmi/client/directory/unit/CompleteMetadata.java
@@ -1,26 +1,66 @@
 package spotify.rmi.client.directory.unit;
-/*
-● tener un cliente “CompleteMetadata” que enriquezca los metadatos (dar like, añadir
-comentarios, tag, score, etc…), a cada uno de los elementos subidos al directorio
-con el objetivo de simular que estamos delante de una plataforma más realista y a
-su vez comprobar que los métodos remotos de estas operaciones funcionan.
- */
+
+import spotify.rmi.common.Spotify;
+import spotify.utils.Directorio;
+import spotify.media.Media;
+
+import java.rmi.Naming;
+import java.util.Random;
+
 public class CompleteMetadata {
-    //Ejemplo
-    public static void main(String [ ] args) {
-        String host = (args.length < 1) ? null : args[0];
+    public static void main(String[] args) {
         try {
-            Hello or = (Hello) Naming.lookup("rmi://"+host+"/id1");
-            String respuesta = or.sayHello();
-            System.out.println("[Respuesta:"+respuesta+"]");
+            String host = "localhost";
+            Spotify spotify = (Spotify) Naming.lookup("rmi://" + host + "/id1");
+
+            System.out.println("Enriqueciendo los metadatos de los elementos en el directorio...");
+            Directorio directorio = new Directorio();
+
+            // Crear un objeto Random para generar datos aleatorios
+            Random random = new Random();
+
+            // Listas de comentarios para elegir aleatoriamente
+            String[] comentarios = {"¡Me encanta!", "¡Excelente canción!", "No está mal.", "Podría ser mejor.",
+                    "¡La mejor canción de todas!", "Increíble.", "Me hace feliz.", "No es mi estilo."};
+
+            for (String clave : directorio.keySet()) {
+                Media media = directorio.obtenerMedia(clave);
+                if (media != null) {
+
+                    // Añadir un número aleatorio de likes (0 a 100)
+                    int likes = random.nextInt(101);
+                    for (int i = 0; i < likes; i++) {
+                        spotify.addLike(clave);
+                    }
+                    System.out.println(likes + " likes añadidos.");
+
+                    // Añadir un número aleatorio de comentarios (0 a 5)
+                    int numComentarios = random.nextInt(6);
+                    for (int i = 0; i < numComentarios; i++) {
+                        String comentario = comentarios[random.nextInt(comentarios.length)];
+                        spotify.addComment(clave, comentario);
+                    }
+                    System.out.println(numComentarios + " comentarios añadidos.");
+
+                    // Añadir tag adulto aleatoriamente (true o false)
+                    boolean isAdult = random.nextBoolean();
+                    spotify.tagAdultContent(clave, isAdult);
+                    System.out.println("Tag adulto añadido: " + isAdult);
+
+                    // Añadir una puntuación aleatoria (0.0 a 10.0)
+                    double score = random.nextDouble() * 10;
+                    spotify.addScore(clave, score);
+                    System.out.println("Score añadido: " + score);
+                }
+            }
+
         } catch (java.rmi.RemoteException re) {
-            System.err.println("<Cliente: Excepción RMI:"+re);
+            System.err.println("<Cliente: Excepción RMI:" + re);
             re.printStackTrace();
 
         } catch (Exception e) {
-            System.err.println("<Cliente: Excepcion: "+e);
+            System.err.println("<Cliente: Excepcion: " + e);
             e.printStackTrace();
         }
     }
-
 }
