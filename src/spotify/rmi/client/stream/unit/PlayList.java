7a4f7eddaced31ec1db489ce66251cfa26b9f85e
David Martin <118901855+DesertZito@users.noreply.github.com>
2024-05-25 23:20:33 +0200

    Update PlayList.java

--- src/spotify/rmi/client/stream/unit/PlayList.java
+++ src/spotify/rmi/client/stream/unit/PlayList.java
@@ -1,75 +1,75 @@
-package spotify.rmi.client.stream.unit;
+    package spotify.rmi.client.stream.unit;
 
-import spotify.rmi.client.stream.SpotifyClientImpl;
-import spotify.rmi.common.Spotify;
-import spotify.media.Media;
-import spotify.rmi.common.SpotifyClient;
-import spotify.rmi.common.SpotifyServer;
-import spotify.rmi.server.SpotifyServerImpl;
-import spotify.utils.Directorio;
+    import spotify.rmi.client.stream.SpotifyClientImpl;
+    import spotify.rmi.common.Spotify;
+    import spotify.media.Media;
+    import spotify.rmi.common.SpotifyClient;
+    import spotify.rmi.common.SpotifyServer;
+    import spotify.rmi.server.SpotifyServerImpl;
+    import spotify.utils.Directorio;
 
-import java.nio.file.attribute.UserPrincipal;
-import java.rmi.Naming;
+    import java.nio.file.attribute.UserPrincipal;
+    import java.rmi.Naming;
 
-public class PlayList {
-    public static void main(String[] args) {
-        try {
-            String host = "localhost";
-            Object or = (Object) Naming.lookup("rmi://" + host + "/id1");
-            SpotifyClient cliente=new SpotifyClientImpl();
+    public class PlayList {
+        public static void main(String[] args) {
+            try {
+                String host = "localhost";
+                Object or = (Object) Naming.lookup("rmi://" + host + "/id1");
+                SpotifyClient cliente=new SpotifyClientImpl();
 
-            if (args.length == 0) {
-                //simular meter varias canciones en una playlist
-                Spotify spotify=(Spotify) or;
-                SpotifyServer server=(SpotifyServer) or;
-                server.setClientStreamReceptor(cliente);
+                if (args.length == 0) {
+                    //simular meter varias canciones en una playlist
+                    Spotify spotify=(Spotify) or;
+                    SpotifyServer server=(SpotifyServer) or;
+                    server.setClientStreamReceptor(cliente);
 
 
-                boolean terminar=false;
-                Media media=spotify.readL();
-                System.out.println(((Spotify) or).hello());
-                java.io.BufferedReader tec =
-                        new java.io.BufferedReader(
-                                new java.io.InputStreamReader(System.in));
-                String linea=null;
-                while (!terminar){
+                    boolean terminar=false;
+                    Media media=spotify.readL();
+                    System.out.println(((Spotify) or).hello());
+                    java.io.BufferedReader tec =
+                            new java.io.BufferedReader(
+                                    new java.io.InputStreamReader(System.in));
+                    String linea=null;
+                    while (!terminar){
 
-                    System.out.println("Que desea hacer, 1-Escuchar la cancion,2-pasar a la siguiente, 3 terminar el proceso");
-                    linea= tec.readLine();
+                        System.out.println("Que desea hacer, 1-Escuchar la cancion,2-pasar a la siguiente, 3 terminar el proceso");
+                        linea= tec.readLine();
 
-                    if(linea.equals("1")){
-                        if(media == null){
-                            System.out.println("PlayList vacia");
+                        if(linea.equals("1")){
+                            if(media == null){
+                                System.out.println("Ninguna cancion seleccionada");
 
+                            }
+                            else {
+
+                                server.startMedia(media);
+                                Thread.sleep(5000);
+                                System.out.print("ESCUCHANDO");
+                                System.out.println(media);
+
+                            }
+                        } else if (linea.equals("2")) {
+                            System.out.println("CANCION ACTUAL "+spotify.peekL());
+                            media=spotify.readL();
                         }
-                        else {
-
-                            server.startMedia(media);
-                            Thread.sleep(5000);
-                            System.out.print("ESCUCHANDO");
-                            System.out.println(media);
-
+                        else{
+                            terminar=true;
                         }
-                    } else if (linea.equals("2")) {
-                        System.out.println("CANCION ACTUAL "+spotify.peekL());
-                        media=spotify.readL();
+
                     }
-                    else{
-                        terminar=true;
-                    }
+                    System.out.println("ADIOS");
+                } else {
 
                 }
-                System.out.println("ADIOS");
-            } else {
+            } catch (java.rmi.RemoteException re) {
+                System.err.println("<Cliente: Excepción RMI:" + re);
+                re.printStackTrace();
 
+            } catch (Exception e) {
+                System.err.println("<Cliente: Excepcion: " + e);
+                e.printStackTrace();
             }
-        } catch (java.rmi.RemoteException re) {
-            System.err.println("<Cliente: Excepción RMI:" + re);
-            re.printStackTrace();
-
-        } catch (Exception e) {
-            System.err.println("<Cliente: Excepcion: " + e);
-            e.printStackTrace();
         }
     }
-}
