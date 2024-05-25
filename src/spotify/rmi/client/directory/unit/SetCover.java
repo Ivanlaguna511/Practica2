7ffa1e1cda4f2ec25e8f046d78e1e7180f0b6693
David Martin <118901855+DesertZito@users.noreply.github.com>
2024-05-25 22:31:09 +0200

    Update SetCover.java

--- src/spotify/rmi/client/directory/unit/SetCover.java
+++ src/spotify/rmi/client/directory/unit/SetCover.java
@@ -2,6 +2,8 @@ package spotify.rmi.client.directory.unit;
 
 import spotify.rmi.common.Spotify;
 import spotify.media.Media;
+
+import javax.swing.*;
 import java.rmi.Naming;
 
 
@@ -11,20 +13,19 @@ public class SetCover {
             if (args.length == 1) {
                 String host = "localhost";
                 Spotify or = (Spotify) Naming.lookup("rmi://" + host + "/id1");
-
-                // Crear un objeto Media con el nombre de la canción
                 String nombreCancion = args[0];
+                // Crear un objeto Media con el nombre de la canción
+
                 Media media = new Media(nombreCancion);
 
                 // Cargar la carátula desde la ruta proporcionada
-                media.loadCover("/home/hecforc/Descargas/DQE.jpeg");
-
+                    media.loadCover("./jpgfiles/" + nombreCancion+ ".jpg");
 
                 // Enviar el objeto Media con la carátula cargada al servidor
                 String respuesta = or.setCover(media);
                 System.out.println("[Respuesta: " + respuesta + " ]");
             } else {
-                System.out.println("Uso incorrecto, uso: SetCover [nombreCancion] [rutaImagen].");
+                System.out.println("Uso incorrecto, uso: SetCover [nombreCancion]");
             }
         } catch (java.rmi.RemoteException re) {
             System.err.println("<Cliente: Excepción RMI:" + re);
