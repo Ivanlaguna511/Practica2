fa8991a2481bd9e14051e02ac2718695f5ce5cf4
David Martin <118901855+DesertZito@users.noreply.github.com>
2024-05-27 16:11:30 +0200

    Update CompleteMetadata.java

--- src/spotify/rmi/client/directory/unit/CompleteMetadata.java
+++ src/spotify/rmi/client/directory/unit/CompleteMetadata.java
@@ -5,64 +5,58 @@ import spotify.utils.Directorio;
 import spotify.media.Media;
 
 import java.rmi.Naming;
-import java.rmi.RemoteException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
+import java.util.StringTokenizer;
 
 public class CompleteMetadata {
     public static void main(String[] args) {
         try {
             String host = "localhost";
-            Spotify or = (Spotify) Naming.lookup("rmi://" + host + "/id1");
+            Spotify spotify = (Spotify) Naming.lookup("rmi://" + host + "/id1");
 
-            // Listas de comentarios para elegir aleatoriamente
-            String [] comentarios = {
-                    "¡Me encanta!",
-                    "¡Excelente canción!",
-                    "No está mal.",
-                    "Podría ser mejor.",
-                    "¡La mejor canción de todas!",
-                    "Increíble.",
-                    "Me hace feliz.",
-                    "No es mi estilo."};
+            System.out.println("Enriqueciendo los metadatos de los elementos en el directorio...");
+            String Lista = spotify.getDirectoryList();
+            StringTokenizer tokenizer = new StringTokenizer(Lista, ",");
 
             // Crear un objeto Random para generar datos aleatorios
             Random random = new Random();
 
-            // Obtener el conjunto de claves del directorio
-            String claves = or.getDirectoryList();
-            if (claves.length() == 0){
-                System.out.println("No se pudo realizar la acción");
-            }
-            else {
-                System.out.println("Enriqueciendo los metadatos de los elementos en el directorio...");
-                String[] palabras = claves.split(", ");
+            // Listas de comentarios para elegir aleatoriamente
+            String[] comentarios = {"¡Me encanta!", "¡Excelente canción!", "No está mal.", "Podría ser mejor.",
+                    "¡La mejor canción de todas!", "Increíble.", "Me hace feliz.", "No es mi estilo."};
+            while (tokenizer.hasMoreTokens()) {
+                String cancion = tokenizer.nextToken();
+                Media media = spotify.retrieveMedia(cancion);
+                if (media != null) {
 
-                for (String clave : palabras) {
                     // Añadir un número aleatorio de likes (0 a 100)
                     int likes = random.nextInt(101);
                     for (int i = 0; i < likes; i++) {
-                        or.addLike(clave);
+                        spotify.addLike(cancion);
                     }
+                    System.out.println(likes + " likes añadidos.");
 
                     // Añadir un número aleatorio de comentarios (0 a 5)
                     int numComentarios = random.nextInt(6);
                     for (int i = 0; i < numComentarios; i++) {
                         String comentario = comentarios[random.nextInt(comentarios.length)];
-                        or.addComment(clave, comentario);
+                        spotify.addComment(cancion, comentario);
                     }
+                    System.out.println(numComentarios + " comentarios añadidos.");
 
                     // Añadir tag adulto aleatoriamente (true o false)
                     boolean isAdult = random.nextBoolean();
-                    or.tagAdultContent(clave, isAdult);
+                    spotify.tagAdultContent(cancion, isAdult);
+                    System.out.println("Tag adulto añadido: " + isAdult);
 
                     // Añadir una puntuación aleatoria (0.0 a 10.0)
                     double score = random.nextDouble() * 10;
-                    or.addScore(clave, score);
+                    spotify.addScore(cancion, score);
+                    System.out.println("Score añadido: " + score);
                 }
             }
-        } catch (RemoteException re) {
+
+        } catch (java.rmi.RemoteException re) {
             System.err.println("<Cliente: Excepción RMI:" + re);
             re.printStackTrace();
 
